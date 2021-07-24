21bf2ed32945ed42d01c0b99d5678b99ef9eaf84
Alejandro Saucedo <axsauze@gmail.com>
2021-07-24 17:05:06 +0100

    Added multidimensional test

--- test_arraylist.zig
+++ test_arraylist.zig
@@ -13,3 +13,19 @@ test "multidimensional arraylist" {
     try std.testing.expect(list.items[0].items[0] == 1);
 }
 
+test "multidimensional arraylist initial size" {
+    const test_allocator = std.testing.allocator;
+    var list = try std.ArrayList(?std.ArrayList(u32)).initCapacity(test_allocator, 10);
+    defer list.deinit();
+    var i: usize = 0;
+    while (i < 10) : (i += 1) {
+        try list.append(null);
+    }
+
+    list.items[3] = std.ArrayList(u32).init(test_allocator);
+    defer list.items[3].?.deinit();
+
+    try list.items[3].?.append(1);
+
+    try std.testing.expect(list.items[3].?.items[0] == 1);
+}
